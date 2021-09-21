2c82c1dbffdef39583f277161a97f5755aaf37e6
Daniel H <naduran@live.se>
2021-09-21 12:12:08 +0200

    Add project files.

---
+++ Övning15Passbokning.web/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Övning15Passbokning.web.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
